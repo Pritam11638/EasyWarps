1fb4d32cf1dfcbfb4e222b0d2a6f912b5ba4d81e
Pritam <pritam2009shaw@gmail.com>
2025-03-22 11:33:53 +0530

    Completed Plugin

---
+++ src/main/java/me/pritamshaw/easyWarps/EasyWarps.java
@@ -0,0 +1,164 @@
+package me.pritamshaw.easyWarps;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents;
+import lombok.Getter;
+import me.pritamshaw.easyWarps.warp.Warp;
+import me.pritamshaw.easyWarps.warp.WarpsManager;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EasyWarps extends JavaPlugin {
+    @Getter
+    private static WarpsManager warpsManager;
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+        saveDefaultConfig();
+
+        warpsManager = new WarpsManager(this);
+
+        getLifecycleManager().registerEventHandler(LifecycleEvents.COMMANDS, eHandler -> {
+            LiteralCommandNode<CommandSourceStack> warpManagerCommand = Commands
+                    .literal("warp-manager")
+                    .requires(cSS -> (cSS.getSender().hasPermission("ew.admin") && cSS.getSender() instanceof Player))
+                    .executes(cSS -> {
+                        cSS.getSource().getSender().sendMessage(Component
+                                .text("Please enter an argument!")
+                                .color(NamedTextColor.RED)
+                                .decoration(TextDecoration.ITALIC, false)
+                        );
+
+                        return Command.SINGLE_SUCCESS;
+                    })
+                    .then(Commands
+                            .literal("add")
+                            .executes(cSS -> {
+                                cSS.getSource().getSender().sendMessage(Component
+                                        .text("Please enter an argument!")
+                                        .color(NamedTextColor.RED)
+                                        .decoration(TextDecoration.ITALIC, false)
+                                );
+
+                                return Command.SINGLE_SUCCESS;
+                            })
+                            .then(Commands
+                                    .argument("warp-name", StringArgumentType.word())
+                                    .executes(cSS -> {
+                                        String warpName = StringArgumentType.getString(cSS, "warp-name");
+
+                                        if (warpsManager.registerNewWarp(new Warp(warpName, cSS.getSource().getLocation()))) {
+                                            cSS.getSource().getSender().sendMessage(Component
+                                                    .text("Created a warp " + warpName + " at your current location!")
+                                                    .color(NamedTextColor.GREEN)
+                                                    .decoration(TextDecoration.ITALIC, false)
+                                            );
+                                        } else {
+                                            cSS.getSource().getSender().sendMessage(Component
+                                                    .text("A warp with that name already exists!")
+                                                    .color(NamedTextColor.RED)
+                                                    .decoration(TextDecoration.ITALIC, false)
+                                            );
+                                        }
+
+                                        return Command.SINGLE_SUCCESS;
+                                    })))
+                    .then(Commands
+                            .literal("remove")
+                            .executes(cSS -> {
+                                cSS.getSource().getSender().sendMessage(Component
+                                        .text("Please enter an argument!")
+                                        .color(NamedTextColor.RED)
+                                        .decoration(TextDecoration.ITALIC, false)
+                                );
+
+                                return Command.SINGLE_SUCCESS;
+                            })
+                            .then(Commands
+                                    .argument("warp-name", StringArgumentType.word())
+                                    .suggests((context, builder) -> {
+                                        warpsManager.getAllWarps().forEach(warp -> builder.suggest(warp.name()));
+
+                                        return builder.buildFuture();
+                                    })
+                                    .executes(cSS -> {
+                                        String warpName = StringArgumentType.getString(cSS, "warp-name");
+
+                                        if (warpsManager.unregisterWarp(warpName)) {
+                                            cSS.getSource().getSender().sendMessage(Component
+                                                    .text("Removed warp " + warpName + "!")
+                                                    .color(NamedTextColor.GREEN)
+                                                    .decoration(TextDecoration.ITALIC, false)
+                                            );
+                                        } else {
+                                            cSS.getSource().getSender().sendMessage(Component
+                                                    .text("A warp with that name doesn't exist!")
+                                                    .color(NamedTextColor.RED)
+                                                    .decoration(TextDecoration.ITALIC, false)
+                                            );
+                                        }
+
+                                        return Command.SINGLE_SUCCESS;
+                                    })
+                            )
+                    )
+                    .build();
+
+            LiteralCommandNode<CommandSourceStack> warpCommand = Commands
+                    .literal("warp")
+                    .requires(cSS -> (cSS.getSender() instanceof Player))
+                    .executes(cSS -> {
+                        cSS.getSource().getSender().sendMessage(Component
+                                .text("Please enter an argument!")
+                                .color(NamedTextColor.RED)
+                                .decoration(TextDecoration.ITALIC, false)
+                        );
+
+                        return Command.SINGLE_SUCCESS;
+                    })
+                    .then(Commands
+                            .argument("warp-name", StringArgumentType.word())
+                            .suggests((context, builder) -> {
+                                warpsManager.getAllWarps().forEach(warp -> builder.suggest(warp.name()));
+
+                                return builder.buildFuture();
+                            })
+                            .executes(cSS -> {
+                                String warpName = cSS.getArgument("warp-name", String.class);
+
+                                if (warpsManager.warpExists(warpName)) {
+                                    warpsManager.warpPlayer((Player) cSS.getSource().getSender(), warpName);
+                                } else {
+                                    cSS.getSource().getSender().sendMessage(Component
+                                            .text("A warp with that name doesn't exist!")
+                                            .color(NamedTextColor.RED)
+                                            .decoration(TextDecoration.ITALIC, false)
+                                    );
+                                }
+
+                                return Command.SINGLE_SUCCESS;
+                            })
+                    )
+                    .build();
+
+            eHandler.registrar().register(warpManagerCommand);
+            eHandler.registrar().register(warpCommand);
+        });
+
+        getLogger().info("EasyWarp has been enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/pritamshaw/easyWarps/warp/WarpsManager.java
@@ -0,0 +1,100 @@
+package me.pritamshaw.easyWarps.warp;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextDecoration;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class WarpsManager {
+    private final Set<Warp> warps = new HashSet<>();
+    private final JavaPlugin plugin;
+
+    public WarpsManager(JavaPlugin pluginInstance) {
+        plugin = pluginInstance;
+
+        FileConfiguration config = plugin.getConfig();
+
+        config.getKeys(false).forEach(key -> warps.add(new Warp(key, config.getLocation(key))));
+    }
+
+    public boolean registerNewWarp(Warp warp) {
+        if (!warpExists(warp)) {
+            warps.add(warp);
+            save();
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean unregisterWarp(Warp warp) {
+        if (warpExists(warp)) {
+            warps.remove(warp);
+            save();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean unregisterWarp(String warpName) {
+        if (warpExists(warpName)) {
+            return unregisterWarp(getWarp(warpName));
+        }
+
+        return false;
+    }
+
+    public Warp getWarp(String warpName) {
+        return warps.stream().filter(warp -> warp.name().equals(warpName)).findFirst().orElse(null);
+    }
+
+    public boolean warpExists(Warp warp) {
+        return warps.contains(warp);
+    }
+
+    public boolean warpExists(String warpName) {
+        return getWarp(warpName) != null;
+    }
+
+    public boolean warpPlayer(Player player, String warpName) {
+        AtomicBoolean found = new AtomicBoolean(false);
+
+        warps.stream().filter(warp -> warp.name().equals(warpName)).findFirst().ifPresent(warp -> {
+            player.sendMessage(
+                    Component
+                            .text("Warping to " + warp.name())
+                            .color(NamedTextColor.GRAY)
+                            .decoration(TextDecoration.ITALIC, false)
+            );
+
+            player.teleport(warp.location());
+
+            found.set(true);
+        });
+
+        return found.get();
+    }
+
+    public Set<Warp> getAllWarps() {
+        return warps;
+    }
+
+    private void save() {
+        FileConfiguration config = plugin.getConfig();
+
+        config.getKeys(false).forEach(warp -> config.set(warp, null));
+
+        warps.forEach(warp -> {
+            config.set(warp.name(), warp.location());
+        });
+
+        plugin.saveConfig();
+    }
+}
